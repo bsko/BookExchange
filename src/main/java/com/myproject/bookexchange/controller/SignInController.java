9deae781bf086066293babfb51231a8dd9d16af6
bsko <adobeangryslave@gmail.com>
2013-06-24 18:15:09 +0400

    added spring mvc controllers

---
+++ src/main/java/com/myproject/bookexchange/controller/SignInController.java
@@ -0,0 +1,10 @@
+package com.myproject.bookexchange.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/signin")
+public class SignInController {
+  
+}
