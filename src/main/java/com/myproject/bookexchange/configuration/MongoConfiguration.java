6f92f1823711047d31451bf51cc16fe45b1805a3
bsko <adobeangryslave@gmail.com>
2013-06-29 19:39:20 +0400

    updated spring configuration

--- src/main/java/com/myproject/bookexchange/configuration/MongoConfiguration.java
+++ src/main/java/com/myproject/bookexchange/configuration/MongoConfiguration.java
@@ -6,6 +6,12 @@ import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import com.mongodb.MongoClient;
+import com.myproject.bookexchange.dao.IBookDAO;
+import com.myproject.bookexchange.dao.IChangeDAO;
+import com.myproject.bookexchange.dao.IUserDAO;
+import com.myproject.bookexchange.dao.impl.BookDAO;
+import com.myproject.bookexchange.dao.impl.ChangeDAO;
+import com.myproject.bookexchange.dao.impl.UserDAO;
 
 @Configuration
 public class MongoConfiguration {
@@ -19,5 +25,26 @@ public class MongoConfiguration {
   MongoTemplate mongoTemplate() throws Exception {
     MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
     return mongoTemplate;
+  }
+  
+  public @Bean 
+    IUserDAO userDAO() throws Exception {
+    IUserDAO dao =  new UserDAO();
+    dao.setMongo(mongoTemplate());
+    return dao;
+  }
+  
+  public @Bean 
+    IBookDAO bookDAO() throws Exception {
+    IBookDAO dao = new BookDAO();
+    dao.setMongo(mongoTemplate());
+    return dao;
+  }
+  
+  public @Bean 
+    IChangeDAO changeDAO() throws Exception {
+    IChangeDAO dao = new ChangeDAO();
+    dao.setMongo(mongoTemplate());
+    return dao;
   }
 }
