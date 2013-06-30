14493581634146cec3b5c56ca97ed7f5ecf18ff7
bsko <adobeangryslave@gmail.com>
2013-06-30 09:09:53 +0400

    added methods implementations in changedao

--- src/main/java/com/myproject/bookexchange/dao/impl/ChangeDAO.java
+++ src/main/java/com/myproject/bookexchange/dao/impl/ChangeDAO.java
@@ -1,7 +1,11 @@
 package com.myproject.bookexchange.dao.impl;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import com.myproject.bookexchange.dao.IChangeDAO;
 import com.myproject.bookexchange.domain.BookVO;
@@ -28,32 +32,40 @@ public class ChangeDAO extends GenericMongoDAO<ChangeVO> implements IChangeDAO {
 
   @Override
   public List<ChangeVO> getChangesByBook(BookVO book) {
-    // TODO Auto-generated method stub
-    return null;
+    Query q = new Query(Criteria.where("book").is(book.getId()));
+    return mongo.find(q, ChangeVO.class);
   }
 
   @Override
   public List<ChangeVO> getChangesByReceiver(UserVO receiver) {
-    // TODO Auto-generated method stub
-    return null;
+    Query q = new Query(Criteria.where("receiver").is(receiver.getId()));
+    return mongo.find(q, ChangeVO.class);
   }
 
   @Override
   public List<ChangeVO> getChangesByGiver(UserVO giver) {
-    // TODO Auto-generated method stub
-    return null;
+    Query q = new Query(Criteria.where("sender").is(giver.getId()));
+    return mongo.find(q, ChangeVO.class);
   }
 
   @Override
   public List<ChangeVO> getAllChangesByUser(UserVO user) {
-    // TODO Auto-generated method stub
-    return null;
+    Query q = new Query(Criteria.where("sender").is(user.getId()).orOperator(Criteria.where("receiver").is(user.getId())));
+    return mongo.find(q, ChangeVO.class);
   }
 
   @Override
   public List<ChangeVO> getAllChangesForDate(Date date) {
-    // TODO Auto-generated method stub
-    return null;
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(date);
+    cal.set(Calendar.HOUR_OF_DAY, 0);
+    cal.set(Calendar.MINUTE, 0);
+    cal.set(Calendar.SECOND, 0);
+    Date from = cal.getTime();
+    cal.add(Calendar.DAY_OF_MONTH, 1);
+    Date to = cal.getTime();
+    Query q = new Query(Criteria.where("date").gte(from).andOperator(Criteria.where("date").lt(to)));
+    return mongo.find(q, ChangeVO.class);
   }
 
 }
